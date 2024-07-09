4c1f73d00074c9f4f6fc7920daf70a15e8065bb4
Jens Klingenberg <mail@jensklingenberg.de>
2024-07-09 23:02:09 +0200

    Fix compile issues

--- Comm Android/build.gradle.kts
+++ Comm Android/build.gradle.kts
@@ -1,6 +1,6 @@
 plugins {
-    id("com.android.application") version "7.3.1" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("com.android.application") version "8.5.0" apply false
+    id("org.jetbrains.kotlin.android") version "2.0.0" apply false
 }
 
 tasks.register("clean", Delete::class) {
